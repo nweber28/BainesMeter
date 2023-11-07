03df15a18d6eb763d722cfc163902cb1cc93ad63
nweber28 <nweber2001@icloud.com>
2023-11-07 10:04:35 -0500

    Migrate to puppeteer

--- public/js/scrape.js
+++ public/js/scrape.js
@@ -1,24 +1,11 @@
-const { By, Key, Builder } = require("selenium-webdriver");
-require("chromedriver");
+const puppeteer = require("puppeteer");
 
-async function example() {
-  var searchString = "Automation testing with Selenium and JavaScript";
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://example.com");
+  const pageTitle = await page.title();
+  console.log("Page title:", pageTitle);
 
-  //To wait for browser to build and launch properly
-  let driver = await new Builder().forBrowser("chrome").build();
-
-  //To fetch http://google.com from the browser with our code.
-  await driver.get("http://google.com");
-
-  //To send a search query by passing the value in searchString.
-  await driver.findElement(By.name("q")).sendKeys(searchString, Key.RETURN);
-
-  //Verify the page title and print it
-  var title = await driver.getTitle();
-  console.log("Title is:", title);
-
-  //It is always a safe practice to quit the browser after execution
-  await driver.quit();
-}
-
-example();
+  await browser.close();
+})();
