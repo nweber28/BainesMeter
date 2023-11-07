36e38f625fa97925473550369db04a16dccb0946
nweber28 <nweber2001@icloud.com>
2023-11-07 16:03:13 -0500

    Scrape Harold Baines only, Set user agent

--- public/js/scrape.js
+++ public/js/scrape.js
@@ -3,13 +3,19 @@ const puppeteer = require("puppeteer");
 (async () => {
   // Launch a headless browser
   const browser = await puppeteer.launch();
+  console.log("launch");
   const page = await browser.newPage();
+  console.log("new page created");
 
+  await page.setUserAgent(
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+  );
+  console.log("user agent set");
   // Navigate to the Baseball-Reference page
   await page.goto(
     "https://www.baseball-reference.com/leaders/WAR_career.shtml"
   );
-
+  console.log("Reached Website");
   // Wait for the table to be visible
   await page.waitForSelector("#leader_standard_WAR");
 
@@ -19,7 +25,7 @@ const puppeteer = require("puppeteer");
     const rows = Array.from(table.querySelectorAll("tbody tr"));
 
     // Slice the array to collect only the first ten rows
-    const slicedRows = rows.slice(0, 10);
+    const slicedRows = rows.slice(585, 586);
 
     return slicedRows.map((row) => {
       const columns = Array.from(row.querySelectorAll("td"));
